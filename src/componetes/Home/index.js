2724ae33eb95458d3438184318d6cc8d8b5d117c
Vanderlei Neto <jv.neto@outlook.com.br>
2023-05-20 01:51:00 -0300

    Ajustes

--- src/componetes/Home/index.js
+++ src/componetes/Home/index.js
@@ -1,18 +1,22 @@
+import React from 'react';
 import "./home.css";
 import Pesquisar from '../Pesquisar';
-const Home = () => {
-    return (
-        <div className="home">
-        <div className='lupa'>
-            <h2 className='barra-de-busca'>Pesquise aqui pelo seu áudio</h2>
-            <Pesquisar />
-        </div>
-        <p>
-        <p className="texto">Lorem ipsum dolor sit amet. Ea obcaecati nesciunt et ipsum voluptatem id ipsum ratione hic commodi quia. Ex tempore eveniet et accusamus beatae sed quisquam elit facilis voluptas ea facere sequi ea quia sapiente aut sint assumenda At porro recusandae ut quas alias. Ad voluptatibus omnis cum consequatur laborum ut nostrum omnis. Quo distinctio quam eum culpa fugiat eos dolorum quibusdam a laborum earum. </p>
 
-        </p>
+const Home = () => {
+  return (
+    <div className="home">
+        <div className='logo'>
+            <img src="/imagens/logo.webp" alt='logo imagem' className='logo-projeto'/>
+        </div>
+      <div className='lupa'>
+        <Pesquisar />
       </div>
-    )
+      <p>
+        
+        <p className="texto">Lorem ipsum dolor sit amet...</p>
+      </p>
+    </div>
+  );
 }
 
 export default Home;
--- src/componetes/RodaPe/index.js
+++ src/componetes/RodaPe/index.js
@@ -2,6 +2,7 @@ import "./rodape.css";
 const RodaPe = () => {
     return (
         <footer className="rodape">
+        <img src="/imagens/logos-eparreifilmes.webp"  alt="Logos de Parceiros" className="logo-patrocinadores"/>
         <p className="texto">
         &copy; Eparrei Filmes
         </p>
